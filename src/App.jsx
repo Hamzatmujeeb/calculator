ed7821083c4e13eb723a96696c6176a5ea348bd5
Hamzat mujeeb <hamzatmujeeb586@gmail.com>
2022-10-31 15:16:34 -0700

    remove unused component module

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 import { Button }  from './components/Button';
 import  {Input} from './components/Input';
